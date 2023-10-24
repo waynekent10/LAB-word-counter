3743d6b039957451c307948761148486fc3fb085
Bruce Clark <bruce.clark55@gmail.com>
2023-10-24 19:19:50 -0400

    got the counter to count

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log("Connected!")
+// YOU WILL MODIFY THIS FUNCTION TO GET THE PROGRAM TO WORK
+const wordCounter = (value) => {
+  // Type into the input and press submit, you will see the value you entered in your console
+  console.log(value);
+
+  if (value) {
+  
+    // The line takes words and splits them by spaces. If I want to count the letters, remove the space in the parenthesis
+    const words = value.split(" ");
+    // This line counts how many words within the box
+    let count = words.length
+    //This line targets the number of words.
+    wordCount.innerHTML = `Word Count: ${count}`;
+
+  } else {
+    // if the value is empty, set the error message value to "Please input text"
+    error.innerHTML = "Please input text"; // UPDATE THIS
+  }
+}
+
+// OPTIONAL CHALLENGE
+const toggleMode = (btnText) => {
+  // complete the function
+}
+
+
+// ************************************************ //
+// **** DO NOT MODIFY THE CODE BELOW THIS LINE **** //
+// ************************************************ //
+
+// These are query selectors. We will focus on them later in the course
+const textarea = document.querySelector("textarea");
+const form = document.querySelector("form");
+const error = document.querySelector("#error");
+const wordCount = document.querySelector("#word-count");
+const toggleButton = document.querySelector("#bg-switch");
+
+// These are event listeners. We will focus on them later in the course
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // the default behavior of a form is to reload the page, we do not want that so we want to prevent that behavior
+  error.innerHTML = ""; // clear out the innerHTML on submit
+  wordCount.innerHTML = ""; // clear out the innerHTML on submit
+  const value = textarea.value; // grab the value of the text area on submit
+  wordCounter(value); // call the function and pass it the value
+});
+
+form.addEventListener("reset", () => {
+  error.innerHTML = ""; // on reset, clear the innderHTML
+  wordCount.innerHTML = ""; // on reset, clear the innderHTML
+});
+
+toggleButton.addEventListener("click", (event) => {
+  toggleMode(event.target.innerHTML);
+});
